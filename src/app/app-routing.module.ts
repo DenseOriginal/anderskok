ef0d3bc5d24f7baa5c32470b5ed7e2f34b3de443
Anders Kok <ak.kornerup@gmail.com>
2021-03-26 20:03:15 +0100

    Created home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
+  },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
