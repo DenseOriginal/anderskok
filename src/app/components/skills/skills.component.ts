005bead6ae7167825ef12121147399a88ef669f0
Anders Kok <ak.kornerup@gmail.com>
2021-03-30 20:37:37 +0200

    Serve skill images from local

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -14,51 +14,51 @@ export class SkillsComponent implements OnInit {
 
   skills: Skill[] = [
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_js_official.svg",
+      imgUrl: "/assets/skills/file_type_js_official.svg",
       name: "JavaScript"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_html.svg",
+      imgUrl: "/assets/skills/file_type_html.svg",
       name: "HTML"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_css.svg",
+      imgUrl: "/assets/skills/file_type_css.svg",
       name: "CSS"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_sass.svg",
+      imgUrl: "/assets/skills/file_type_sass.svg",
       name: "SCSS"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_node.svg",
+      imgUrl: "/assets/skills/file_type_node.svg",
       name: "NodeJS"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_angular.svg",
+      imgUrl: "/assets/skills/file_type_angular.svg",
       name: "Angular"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_vue.svg",
+      imgUrl: "/assets/skills/file_type_vue.svg",
       name: "Vue"
     },
     {
-      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png",
+      imgUrl: "/assets/skills/github.svg",
       name: "GitHub"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_netlify.svg",
+      imgUrl: "/assets/skills/file_type_netlify.svg",
       name: "Netlify"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_typescript_official.svg",
+      imgUrl: "/assets/skills/file_type_typescript_official.svg",
       name: "TypeScript"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_firebase.svg",
+      imgUrl: "/assets/skills/file_type_firebase.svg",
       name: "Firebase"
     },
     {
-      imgUrl: "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/file_type_mongo.svg",
+      imgUrl: "/assets/skills/file_type_mongo.svg",
       name: "MongoDB"
     },
   ];
