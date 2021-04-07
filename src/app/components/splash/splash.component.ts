78f69609500c615f90b5a6f53614605b857d1dad
Anders Kok <ak.kornerup@gmail.com>
2021-04-07 22:24:23 +0200

    Added splash screen

--- src/app/components/splash/splash.component.ts
+++ src/app/components/splash/splash.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-splash',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashComponent implements OnInit {
 
+  @Output() done = new EventEmitter();
+  animate = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.animate = true, 500);
   }
 
+  animationEnd(e: AnimationEvent) {
+    if(e.animationName != "splash") return;
+    console.log('Splash done');
+    this.done.emit();
+  }
 }
