0f4517af370b22fb5e02c311e0d99e266afe9b50
Anders Kok <ak.kornerup@gmail.com>
2021-04-09 19:22:39 +0200

    Removing mat button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgWhiteboardModule } from 'ng-whiteboard';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatButtonModule } from '@angular/material/button';
 // import { MatIconModule } from '@angular/material/icon';
 import { ComponentModule } from "./components/components.module";
 import { HomeComponent } from './pages/home/home.component';
@@ -22,7 +22,7 @@ import { HomeComponent } from './pages/home/home.component';
     BrowserAnimationsModule,
     NgWhiteboardModule,
     LayoutModule,
-    MatButtonModule,
+    // MatButtonModule,
     // MatIconModule,
     ComponentModule,
   ],
