32b821ad12d89c2a985867b74826b79dd44ed1d6
Anders Kok <ak.kornerup@gmail.com>
2021-04-09 10:24:51 +0200

    Bound doodle color to css variable

--- src/app/components/doodle-controller/doodle-controller.component.ts
+++ src/app/components/doodle-controller/doodle-controller.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { tap } from 'rxjs/operators';
 import { DoodleSubmitComponent } from '../doodle-submit/doodle-submit.component';
 
 @Component({
@@ -12,10 +13,21 @@ export class DoodleControllerComponent implements OnInit {
   @Input() color: string = "#212121";
   @Output() colorChange = new EventEmitter<string>();
   @Output() undo = new EventEmitter<boolean>();
+  
+  doodleColor1 = getComputedStyle(document.body).getPropertyValue('--doodle-color-1').trim();
+  doodleColor2 = getComputedStyle(document.body).getPropertyValue('--doodle-color-2').trim();
+  doodleColor3 = getComputedStyle(document.body).getPropertyValue('--doodle-color-3').trim();
+  doodleColor4 = getComputedStyle(document.body).getPropertyValue('--doodle-color-4').trim();
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    console.log(this.doodleColor1);
+    console.log(this.doodleColor2);
+    console.log(this.doodleColor3);
+    console.log(this.doodleColor4);
+
+    this.colorChange.subscribe(console.log);
   }
 
   submitDoodle() {
